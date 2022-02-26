7402df910333a01e0de107903f74e704603b3615
Kennethcodes <39102148+Snakeybob@users.noreply.github.com>
2022-02-26 10:40:28 +0800

    Update SQL Project Planning.sql

--- SQL/Advanced Join/SQL Project Planning.sql
+++ SQL/Advanced Join/SQL Project Planning.sql
@@ -21,3 +21,29 @@ FROM (SELECT start_date FROM projects
 WHERE start_date < end_date
 GROUP BY start_date
 ORDER BY MIN(end_date) - start_date, start_date;
+
+/* 
+I propose the following solution as an alternative.
+Basically I assigned an index to each row and use that to join the two tables.
+The rest is similar.
+*/
+SELECT
+    s.start_date,
+    e.end_date
+FROM
+    (
+    SELECT start_date, ROW_NUMBER() OVER (ORDER BY start_date) AS rn
+    FROM projects
+    WHERE start_date NOT IN (SELECT end_date FROM projects
+    ORDER BY start_date 
+    )) AS s
+JOIN    
+    (
+    SELECT end_date, ROW_NUMBER() OVER (ORDER BY end_date) AS rn
+    FROM projects
+    WHERE end_date NOT IN (SELECT start_date FROM projects
+    ORDER BY end_date
+    )) AS e
+ON s.rn = e.rn    
+ORDER BY DATEDIFF(s.start_date, e.end_date) DESC, s.start_date
+; 
